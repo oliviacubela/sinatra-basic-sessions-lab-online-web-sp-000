072dda4689fdc84913ae8e2504b2b652b9041f12
Olivia Cubela <oliviacubela@gmail.com>
2020-09-28 15:43:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session["item"] = params[:item]
+    @session = session
+    erb :'checkout/display' 
+  end
+
+  configure do
+    enable :sessions
+    set :session_secret, "2116multnomah1995"
+  end
 end
