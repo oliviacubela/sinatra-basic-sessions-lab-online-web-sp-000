55e90922788429273e2a013c18ff0e14acea3813
Olivia Cubela <oliviacubela@gmail.com>
2020-09-28 15:15:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-
+    
   end
   #
-  # configure do
-  #   enable :sessions
-  #   set :session_secret, "2116multnomah1995"
-  # end
+  configure do
+    enable :sessions
+    set :session_secret, "2116multnomah1995"
+  end
 end
 
 # get '/hey' do
